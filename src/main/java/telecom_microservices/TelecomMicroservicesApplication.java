4f8371ea2c8519970bfd90ee48f6630c1a1233b2
Yraly_S <eralysagyngali@gmail.com>
2026-06-08 18:23:27 +0500

    start project and set docker compose

---
+++ src/main/java/telecom_microservices/TelecomMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package telecom_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/telecom_microservices/TelecomMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package telecom_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
