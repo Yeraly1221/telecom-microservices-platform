144c859aa6ee2cbaaa4c9f4ffb31c97a8bfb2508
Yraly_S <eralysagyngali@gmail.com>
2026-06-09 21:23:11 +0500

    started auth

---
+++ auth-service/src/main/java/com/example/CredentialsRepository.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CredentialsRepository extends JpaRepository<Credentials, Long> {
+
+    Optional<Credentials> findByEmail(String email);
+}
